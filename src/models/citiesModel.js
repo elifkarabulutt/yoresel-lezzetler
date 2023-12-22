68991c892b35a62cc074552ae1ee0fb990dccb3e
Arif Özcan <zcanarif@gmail.com>
2023-12-22 22:08:51 +0300

    Update citiesModel.js

--- src/models/citiesModel.js
+++ src/models/citiesModel.js
@@ -1,1 +1,36 @@
+// try catch olmadan
+
+const getCities = async (pool) => {
+  const result = await pool.query('SELECT * FROM cities');
+  return result.rows;
+};
+
+module.exports = { getCities };
+
+
+
+
+
+
+
+
+
+
+
+/*
+const getCities = async () => {
+  try {
+    const result = await pool.query('SELECT * FROM cities');
+    return result.rows;
+  } catch (error) {
+    console.error('Error fetching cities', error);
+    throw error;
+  }
+};
+module.exports = { getCities };
+*/
+
+
+
+
 
