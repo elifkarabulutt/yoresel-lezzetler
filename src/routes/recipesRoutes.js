809f956e5fb9be621e1302867b6eccac0ef75768
Arif Özcan <zcanarif@gmail.com>
2023-12-22 22:09:29 +0300

    Update recipesRoutes.js

--- src/routes/recipesRoutes.js
+++ src/routes/recipesRoutes.js
@@ -1,1 +1,15 @@
 
+const express = require('express');
+const recipesController = require('../controllers/recipesController');
+
+const router = express.Router();
+
+// Tarifleri getiren endpoint
+router.get('/', recipesController.getRecipes);
+
+// Bir tarifin detaylarını getiren endpoint
+router.get('/:id', recipesController.getRecipeDetails);
+
+module.exports = router;
+
+
