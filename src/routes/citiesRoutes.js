a022101da2456aa1b62a10a144afd8a2a4dd81c3
Arif Özcan <zcanarif@gmail.com>
2023-12-22 22:09:16 +0300

    Update citiesRoutes.js

--- src/routes/citiesRoutes.js
+++ src/routes/citiesRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const citiesController = require('../controllers/citiesController');
+
+const router = express.Router();
+
+// Şehirleri getiren endpoint
+router.get('/', citiesController.getCities);
+
+module.exports = router;
+
+
 
